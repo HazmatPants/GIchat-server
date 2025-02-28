b23b364938d4f552e741a9cce8801a84dd38eacd
HazmatPants <freddyhodgson1973@gmail.com>
2025-02-28 19:19:15 +0000

    add blacklist function

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
-import asyncio 
+import asyncio
 import websockets
 import time
 import json
 
-host = "0.0.0.0"
-port = 8765
+def load_blacklist(file_path):
+    with open(file_path, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+BLACKLIST = load_blacklist("blacklist.txt")
+
+SRV_CONFIG = json.loads(open("config.json", "r").read())
+SRV_VERSION = "1.3"
+SRV_STARTTIME = time.time()
+
+host = SRV_CONFIG["host"]
+port = SRV_CONFIG["port"]
 
 connected_clients = set()
+
+def filter_message(message):
+    for word in BLACKLIST:
+        message = message.replace(word, "****")
+        return message
 
 async def echo(websocket):
     username = await websocket.recv()
@@ -42,6 +57,13 @@ async def echo(websocket):
                             "event": "srv_message"
                             }
                         await websocket.send(json.dumps(data))
+                    elif user_message.strip() == "/srv.info":
+                        data = {
+                            "username": "server",
+                            "message": f"Server Version: {SRV_VERSION}, Uptime: {time.time() - SRV_STARTTIME}",
+                            "event": "srv_message"
+                            }
+                        await websocket.send(json.dumps(data))
                     else:
                         data = {
                             "username": "server",
@@ -52,6 +74,8 @@ async def echo(websocket):
                 else:
                     for client in connected_clients:
                         if client[1] != websocket:
+                            if SRV_CONFIG["block_bad_words"]:
+                                message_data["message"] = filter_message(message_data["message"])
                             await client[1].send(json.dumps(message_data))
                     print(f"Sent data to all: {message_data}")
             except json.JSONDecodeError:
